189429e71dea07841b6df4cdad7eb6b7f94d363e
flashdrag <flashdrag@gmail.com>
2024-01-17 15:34:03 +0000

    Add Vue Router for pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-  </div>
+  <h1>Hello</h1>
+  <router-view />
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {
-  }
+  components: {}
 }
 </script>
 
