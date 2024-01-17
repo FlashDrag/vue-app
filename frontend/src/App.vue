a9592b35d76a51fc3556c36f3fcff59a5f184be5
flashdrag <flashdrag@gmail.com>
2024-01-17 18:13:02 +0000

    Add NavBar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <h1>Two Trees Olive Oil</h1>
+  <NavBar />
   <div class="page-wrap">
     <router-view />
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar';
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    NavBar,
+  },
 };
 </script>
