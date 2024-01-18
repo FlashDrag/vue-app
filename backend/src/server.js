bc5229e07f65b7700c4ae74884df0166ccd08138
flashdrag <flashdrag@gmail.com>
2024-01-18 17:19:56 +0000

    Refactor server.js and temp-data.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { cartItems as cartItemsRaw, products as productsRaw } from "./temp-data";
+import {
+  cartItems as cartItemsRaw,
+  products as productsRaw,
+} from "./temp-data";
 
 let cartItems = cartItemsRaw;
 let products = productsRaw;
@@ -16,8 +19,13 @@ app.get("/products", (req, res) => {
   res.json(products);
 });
 
+function populateCartIds(ids) {
+  return cartItems.map((id) => products.find((product) => product.id === id));
+}
+
 app.get("/cart", (req, res) => {
-  res.json(cartItems);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
 });
 
 app.get("/products/:productId", (req, res) => {
@@ -28,16 +36,17 @@ app.get("/products/:productId", (req, res) => {
 
 app.post("/cart", (req, res) => {
   const productId = req.body.id;
-  const product = products.find((product) => product.id === productId);
-  cartItems.push(product);
-  res.json(cartItems);
+  cartItems.push(productId);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
 });
 
 app.delete("/cart/:productId", (req, res) => {
   const productId = req.params.productId;
-   cartItems = cartItems.filter(product => product.id !== productId);
-  res.json(cartItems);
-})
+  cartItems = cartItems.filter((id) => id !== productId);
+  const populatedCart = populateCartIds(cartItems);
+  res.json(populatedCart);
+});
 
 app.listen(8000, () => {
   console.log("Server is running on port: 8000");
