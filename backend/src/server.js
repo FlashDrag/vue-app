c6911d8667f3a1c0df533e7aee4fa8eabac36c28
flashdrag <flashdrag@gmail.com>
2024-01-19 11:35:59 +0000

    Refactor server.js to connect do db once

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,64 +4,70 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
+async function start() {
+  const client = new MongoClient(process.env.MONGO_DB_URL);
 
-const client = new MongoClient(process.env.MONGO_DB_URL);
-const app = express();
-app.use(express.json());
-
-// endpoint for testing
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/products", async (req, res) => {
-  try {
-    await client.connect();
-    // connect to the database "fsv-db"
-    const db = client.db("fsv-db");
-    // get the collection "products" and convert it to an array
-    const products = await db.collection("products").find().toArray();
-    res.json(products);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-async function populateCartIds(ids) {
+  // connect to the database "fsv-db"
   await client.connect();
   const db = client.db("fsv-db");
-  return Promise.all(ids.map(id => db.collection("products").findOne({ id })));
+
+  const app = express();
+  app.use(express.json());
+
+  // endpoint for testing
+  app.get("/hello", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.get("/products", async (req, res) => {
+    try {
+      // get the collection "products" and convert it to an array
+      const products = await db.collection("products").find().toArray();
+      res.json(products);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  async function populateCartIds(ids) {
+    return Promise.all(
+      ids.map((id) => db.collection("products").findOne({ id }))
+    );
+  }
+
+  app.get("/users/:userId/cart", async (req, res) => {
+    const user = await db
+      .collection("users")
+      .findOne({ id: req.params.userId });
+    const populatedCart = await populateCartIds(user.cartItems);
+    res.json(populatedCart);
+  });
+
+  app.get("/products/:productId", async (req, res) => {
+    const product = await db
+      .collection("products")
+      .findOne({ id: req.params.productId });
+    res.json(product);
+  });
+
+  app.post("/cart", (req, res) => {
+    const productId = req.body.id;
+    cartItems.push(productId);
+    const populatedCart = populateCartIds(cartItems);
+    res.json(populatedCart);
+  });
+
+  app.delete("/cart/:productId", (req, res) => {
+    const productId = req.params.productId;
+    cartItems = cartItems.filter((id) => id !== productId);
+    const populatedCart = populateCartIds(cartItems);
+    res.json(populatedCart);
+  });
+
+  app.listen(3000, () => {
+    console.log("Server is running on port: 3000");
+  });
 }
 
-app.get("/users/:userId/cart", async (req, res) => {
-  await client.connect();
-  const db = client.db("fsv-db");
-  const user = await db.collection("users").findOne({ id: req.params.userId });
-  const populatedCart = await populateCartIds(user.cartItems);
-  res.json(populatedCart);
-});
 
-app.get("/products/:productId", async (req, res) => {
-  await client.connect();
-  const db = client.db("fsv-db");
-  const product = await db.collection("products").findOne({ id: req.params.productId})
-  res.json(product);
-});
-
-app.post("/cart", (req, res) => {
-  const productId = req.body.id;
-  cartItems.push(productId);
-  const populatedCart = populateCartIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.delete("/cart/:productId", (req, res) => {
-  const productId = req.params.productId;
-  cartItems = cartItems.filter((id) => id !== productId);
-  const populatedCart = populateCartIds(cartItems);
-  res.json(populatedCart);
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port: 3000");
-});
+start();
