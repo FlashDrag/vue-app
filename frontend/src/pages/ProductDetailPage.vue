e6f9effe4f646797c958db7f40adfdc7e9dcc7c0
flashdrag <flashdrag@gmail.com>
2024-01-19 14:15:44 +0000

    Add disabled state to "Add to cart" button

--- frontend/src/pages/ProductDetailPage.vue
+++ frontend/src/pages/ProductDetailPage.vue
@@ -6,7 +6,9 @@
     <div class="product-details">
       <h1>{{ product.name }}</h1>
       <h3 class="price">{{ product.price }}</h3>
-      <button @click="addToCart" class="add-to-cart">Add to cart</button>
+      <button :disabled="itemIsInCart" @click="addToCart" class="add-to-cart">
+        Add to cart
+      </button>
     </div>
   </div>
   <div v-else>
@@ -26,17 +28,32 @@ export default {
   data() {
     return {
       product: {},
+      cartItems: [],
     };
   },
   methods: {
     async addToCart() {
-      await axios.post("/api/users/1/cart", { id: this.$route.params.productId });
+      await axios.post("/api/users/1/cart", {
+        id: this.$route.params.productId,
+      });
       alert("Successfully added item to cart!");
-    }
+    },
   },
   async created() {
-    const response = await axios.get(`/api/products/${this.$route.params.productId}`);
+    const response = await axios.get(
+      `/api/products/${this.$route.params.productId}`
+    );
     this.product = response.data;
-  }
+
+    const cartResponse = await axios.get("/api/users/1/cart");
+    this.cartItems = cartResponse.data;
+  },
+  computed: {
+    itemIsInCart() {
+      return this.cartItems.some(
+        (item) => item.id === this.$route.params.productId
+      );
+    },
+  },
 };
 </script>
