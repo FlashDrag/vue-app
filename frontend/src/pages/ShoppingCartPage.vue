f1d38cf359426b9b661018cc5a659668f9119ae8
flashdrag <flashdrag@gmail.com>
2024-01-19 13:39:39 +0000

    Add remove functionality to Shopping Cart

--- frontend/src/pages/ShoppingCartPage.vue
+++ frontend/src/pages/ShoppingCartPage.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Shopping Cart</h1>
   <div v-if="cartItems.length">
-    <ShoppingCartList :products="cartItems" />
+    <ShoppingCartList @remove-from-cart="removeFromCart($event)" :products="cartItems" />
     <button class="checkout-button">Proceed to Checkout</button>
   </div>
   <div v-else>
@@ -22,6 +22,12 @@ export default {
     return {
       cartItems: [],
     };
+  },
+  methods: {
+    async removeFromCart(productId){
+      const response = await axios.delete(`/api/users/1/cart/${productId}`);
+      this.cartItems = response.data;
+    },
   },
   async created() {
     const response = await axios.get('/api/users/1/cart');
